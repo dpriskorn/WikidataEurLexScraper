0c942b145a7186efaa0074610a46aaa4c0873915
Dennis Priskorn <egils@riseup.net>
2023-12-17 15:57:53 +0100

    refactor: Refactor monster method

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from bs4 import BeautifulSoup
 from ftlangdetect import detect
 from pydantic import BaseModel
 from wikibaseintegrator import WikibaseIntegrator
+from wikibaseintegrator.entities import ItemEntity
 from wikibaseintegrator.wbi_config import config as wbconfig
 from wikibaseintegrator.wbi_helpers import execute_sparql_query
 from wikibaseintegrator.wbi_login import Login
@@ -42,9 +43,13 @@ class Title(BaseModel):
             logger.info(f"got good title for language {self.detected_language}")
         else:
             if self.languages_match:
-                logger.info(f"got title we do not accept because of low score of {self.score} for {self.detected_language}")
+                logger.info(
+                    f"got title we do not accept because of low score of {self.score} for {self.detected_language}"
+                )
             else:
-                logger.info(f"Eur-Lex language {self.language} and detected language {self.detected_language} did not match")
+                logger.info(
+                    f"Eur-Lex language {self.language} and detected language {self.detected_language} did not match"
+                )
                 logger.debug(f"for title {self.title} with score: {self.score}")
 
     @property
@@ -63,6 +68,8 @@ class Title(BaseModel):
 
 
 class LawItem(BaseModel):
+    something_to_upload = False
+    item: ItemEntity = None
     wbi: WikibaseIntegrator
     item_id: str
     celex_id: str
@@ -102,9 +109,16 @@ class LawItem(BaseModel):
         self.enrich_wikidata()
 
     def enrich_wikidata(self):
-        something_to_upload = False
-        item = self.wbi.item.get(entity_id=self.item_id)
-        print(item.get_entity_url())
+        self.item = self.wbi.item.get(entity_id=self.item_id)
+        print(self.item.get_entity_url())
+        self.iterate_languages()
+        if self.something_to_upload:
+            input("press enter to upload")
+            self.item.write(
+                summary="Adding labels and aliases " "with WikidataEurLexScraper"
+            )
+
+    def iterate_languages(self):
         for language in self.languages:
             language_lower = language.lower()
             title_already_in_wikidata = False
@@ -117,45 +131,48 @@ class LawItem(BaseModel):
                     else:
                         title_for_this_language = title
             if title_for_this_language is not None:
-                label = item.labels.get(language=language.lower())
+                label = self.item.labels.get(language=language.lower())
                 if label:
                     has_label = True
                     if label == title_for_this_language.title:
-                        logger.info(f"label for {language_lower} in "
-                                    "wikidata mathches the title, skipping this language")
+                        logger.info(
+                            f"label for {language_lower} in "
+                            "wikidata mathches the title, skipping this language"
+                        )
                         title_already_in_wikidata = True
                     if not title_already_in_wikidata:
                         logger.info(f"checking {language_lower} aliases")
-                        aliases = item.aliases.get(language=language_lower)
+                        aliases = self.item.aliases.get(language=language_lower)
                         if aliases:
                             logger.info(aliases)
                             for alias in aliases:
                                 if alias == title_for_this_language.title:
                                     title_already_in_wikidata = True
                 if not title_already_in_wikidata:  # and title_for_this_language.title
-                    something_to_upload = True
+                    self.something_to_upload = True
                     # we are missing this data in Wikidata, lets add it
                     if not has_label:
                         # add as label
-                        item.labels.set(value=title_for_this_language.title,
-                                        language=language_lower)
+                        self.item.labels.set(
+                            value=title_for_this_language.title, language=language_lower
+                        )
                     else:
                         # add as alias
-                        item.aliases.set(values=[title_for_this_language.title],
-                                         language=language_lower)
+                        self.item.aliases.set(
+                            values=[title_for_this_language.title],
+                            language=language_lower,
+                        )
                 else:
                     logger.info("this title is already in Wikidata")
-        if something_to_upload:
-            input("press enter to upload")
-            item.write(summary="Adding labels and aliases "
-                               "with WikidataEurLexScraper")
 
     def scrape_law_titles(self):
         print(f"Fetching law titles for {self.celex_id}")
         for language in self.languages:
             # Construct the URL based on the CELEX identifier
-            url = (f"https://eur-lex.europa.eu/legal-content/{language}"
-                   f"/TXT/?uri=CELEX:{self.celex_id}")
+            url = (
+                f"https://eur-lex.europa.eu/legal-content/{language}"
+                f"/TXT/?uri=CELEX:{self.celex_id}"
+            )
             logger.info(f"Fetching {url}")
             # Send a GET request to the URL
             response = requests.get(url)
@@ -207,9 +224,9 @@ class EurlexScraper(BaseModel):
         query = execute_sparql_query(self.sparql_query)
 
         # Fetching items from the query result
-        for result in query['results']['bindings']:
-            item_id = self.get_stripped_qid(item_id=str(result['item']['value']))
-            celex_id = result['celex_id']['value']
+        for result in query["results"]["bindings"]:
+            item_id = self.get_stripped_qid(item_id=str(result["item"]["value"]))
+            celex_id = result["celex_id"]["value"]
             self.items.append(LawItem(item_id=item_id, celex_id=celex_id, wbi=self.wbi))
 
     def iterate_items(self):
@@ -223,7 +240,9 @@ class EurlexScraper(BaseModel):
                 print(f"{item.item_id} has already been processed")
 
     def already_processed(self, item_id) -> bool:
-        self.cursor.execute('SELECT COUNT(item_id) FROM processed WHERE item_id = ?', (item_id,))
+        self.cursor.execute(
+            "SELECT COUNT(item_id) FROM processed WHERE item_id = ?", (item_id,)
+        )
         count = self.cursor.fetchone()[0]
         return bool(count > 0)
 
@@ -233,7 +252,7 @@ class EurlexScraper(BaseModel):
 
     def connect(self):
         # Connect to a database (creates if not exists)
-        self.conn = sqlite3.connect('database.db')
+        self.conn = sqlite3.connect("database.db")
 
     def get_cursor(self):
         # Create a cursor object to execute SQL commands
@@ -241,41 +260,48 @@ class EurlexScraper(BaseModel):
 
     def create_db(self):
         # Create a table named 'processed' with a column 'item_id'
-        self.cursor.execute('''
+        self.cursor.execute(
+            """
             CREATE TABLE IF NOT EXISTS processed (
                 item_id INTEGER PRIMARY KEY
             )
-        ''')
+        """
+        )
 
         # Create an index on the 'item_id' column
-        self.cursor.execute('''
+        self.cursor.execute(
+            """
             CREATE INDEX IF NOT EXISTS idx_item_id ON processed (item_id)
-        ''')
+        """
+        )
 
         # Commit changes and close the connection
         self.conn.commit()
 
     def add_item_id_to_database(self, item_id: int):
         # try:
-        self.cursor.execute('''
+        self.cursor.execute(
+            """
             INSERT INTO processed (item_id) VALUES (?)
-        ''', (item_id,))
+        """,
+            (item_id,),
+        )
         # except IntegrityError:
         #     # skip silently if item_id already exists
         #     pass
         self.conn.commit()
 
     def get_count_of_done_item_ids(self):
-        self.cursor.execute('SELECT COUNT(item_id) FROM processed')
+        self.cursor.execute("SELECT COUNT(item_id) FROM processed")
         count = self.cursor.fetchone()[0]
         print(f"{count} item_ids found in the database")
 
 
 # Example usage:
 # celex_id = "32012L0013"
-wbi = WikibaseIntegrator(login=Login(
-    user=config.user_name,
-    password=config.bot_password))
+wbi = WikibaseIntegrator(
+    login=Login(user=config.user_name, password=config.bot_password)
+)
 scraper = EurlexScraper(wbi=wbi)
 scraper.start()
 # item = LawItem(item_id="", celex_id=celex_id, wbi=wbi)
