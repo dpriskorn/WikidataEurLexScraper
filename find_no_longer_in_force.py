0332b2a7ab88d5c48a37d441e0ab864a3142c4c9
Dennis Priskorn <egils@riseup.net>
2024-01-02 23:14:18 +0100

    feat: Support fetching end of validity date data from CELLAR via SPARQL

---
+++ find_no_longer_in_force.py
@@ -0,0 +1,11 @@
+#https://pypi.org/project/pyeurlex/
+import pandas as pd
+
+from eurlex.eurlex import Eurlex
+eur = Eurlex()
+q = eur.make_query(resource_type = "directive", order = True, limit = 10, include_date_endvalid=True, include_force=True, include_date_force=True)
+print(q)
+d = eur.query_eurlex(q)  # where q is a query generated in a previous step or a string defined by you
+d.to_csv(path_or_buf="test.csv")
+pd.set_option('display.max_colwidth', None)  # Set to display the full content of columns without truncation
+print(d)
